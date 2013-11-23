18c91d39b7fa196a761cf2f02482fd8b2ba22d47
aloktiagi <aloktiagi@gmail.com>
2013-11-23 12:54:52 -0800

    prints added

--- switchflow.py
+++ switchflow.py
@@ -9,7 +9,7 @@ srcmac = "4e:ce:8c:e1:29:aa"
 dstmac = "fa:91:d5:03:4e:9c"
 
 #switchlist[0]
-print "First switch configure only external ports"
+print 'First switch configure only external ports {}'.format(switchlist[0])
 fo = open("/tmp/flow", "wb")
 fo.write("table=1, priority=100,dl_vlan=10, dl_dst=");
 fo.write(dstmac);
@@ -32,7 +32,7 @@ fo.close()
 subprocess.call(['sudo', 'ovs-ofctl', 'add-flows', switchlist[0], '/tmp/flow'])
 
 #switchlist[last one]
-print "Last switch configure only external ports"
+print 'Last switch configure only external ports {}'.format(switchlist[len(switchlist)-1])
 fo = open("/tmp/flow", "wb")
 fo.write("table=1, priority=100,dl_vlan=10, dl_dst=");
 #srcmac is the dst mac for the last switch
@@ -56,6 +56,7 @@ fo.close()
 subprocess.call(['sudo', 'ovs-ofctl', 'add-flows', switchlist[len(switchlist)-1], '/tmp/flow'])
 
 for i in range(1,len(switchlist) - 1):
+    print 'Configuring intermediate switch {}'.format(switchlist[i])
     fo = open("/tmp/flow", "wb")
     fo.write("table=0, priority=99,in_port=");
     #TODO get port connected to switchlist[i-1] 
@@ -92,7 +93,3 @@ for i in range(1,len(switchlist) - 1):
 
     fo.close()
     subprocess.call(['sudo', 'ovs-ofctl', 'add-flows', switchlist[i], '/tmp/flow'])
-
-    print switchlist[i]
-
-
