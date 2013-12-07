ccf64a608514339ab13e2170fa1a469e26bf5191
John Bellessa <johnbellessa@gmail.com>
2013-12-07 13:36:08 -0600

    Cretated utils module to keep things clean

---
+++ vnetmanager/utils.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+import socket
+import struct
+import random
+
+def now():
+    return datetime.utcnow()
+
+def ipToInteger(ipAddr):
+    return struct.unpack("!I", socket.inet_aton(ipAddr))[0]
+
+def integerToIp(intIpAddr):
+    return socket.inet_ntoa(struct.pack("!I", intIpAddr))
+
+
+def generateMAC():
+    macInteger = random.getrandbits(48)
+    macInteger  = macInteger | (1<<42)
+    macInteger = macInteger & int('111111101111111111111111111111111111111111111111',2)
+    macString = str(hex(macInteger))
+    blocks = [macString[x:x+2] for x in xrange(0, len(macString), 2)]
+    macFormatted = ':'.join(blocks)
+    return macFormatted.rstrip("-L").lstrip("0x-")
